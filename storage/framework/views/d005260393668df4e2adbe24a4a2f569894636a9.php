9ac783e8f9bcaf7c45d3c3350fdf45ca34296476
etienne wayne <etiennewayne@gmail.com>
2023-05-20 11:42:52 +0800

    update repo with storage

---
+++ storage/framework/views/d005260393668df4e2adbe24a4a2f569894636a9.php
@@ -0,0 +1,7 @@
+
+
+<?php $__env->startSection('content'); ?>
+    <inventories></inventories>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\eshen\Desktop\Github\vinz_posinv\resources\views/administrator/inventories.blade.php ENDPATH**/ ?>
